a75bf6899d6280f3096196c0b2cc4d524f831819
advaith <advaith.madhukar@gmail.com>
2020-04-12 19:02:53 +0530

    Revert "added registration. prepartation for django q"
    This reverts commit 2cc5282f674bb9a10448c245a9aea7fa0f0bb857.

--- library/urls.py
+++ library/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,16 +25,5 @@ urlpatterns = [
     path('database/', include('database.urls')),
     path('library/', include('database.urls')),
     path('admin_panel/', include("costs.urls")),
-    path('error/',include('database.urls')),
-    path('registration/', include('reg.urls')),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
-        name='password_change_done'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), 
-        name='password_change'),
-    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
-     name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-     name='password_reset_complete'),
+    path('error/',include('database.urls'))
 ]
